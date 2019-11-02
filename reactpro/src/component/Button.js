00bcd2a1d79d2a2a81ebd57ce5c78a99e60d8bbb
Tianya Liu <tianyaliu1995@gmail.com>
2019-11-02 00:06:10 -0400

    addToCart button updated

--- reactpro/src/component/Button.js
+++ reactpro/src/component/Button.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import App from '../App.css';
 
 export const ButtonContainer = styled.button`
 	/*directly write css inside*/
@@ -6,14 +7,18 @@ export const ButtonContainer = styled.button`
 	font-size: 1.2rem;
 	background: transparent;
 	border: 0.05rem solid var(--lightBlue);
-	color: var(--lightBlue);
+	border-color: ${props => 
+		props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+	color: ${prop => 
+		prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 	border-radius: 0.5rem;
 	padding: 0.2rem 0.5rem;
 	cursor: pointer;
 	margin: 0.2rem 0.5rem 0.2rem 0;
 	transition: all 0.5s ease-in-out;
 	&:hover {
-		background: var(--lightBlue);
+		background: ${prop => 
+			prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 		color: var(--mainBlue);
 	}
 	&:focus {
