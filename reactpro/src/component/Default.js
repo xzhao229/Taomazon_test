30f3a6732279777edc5424246ce9efd5840fed24
Tianya Liu <tianyaliu1995@gmail.com>
2019-11-07 23:36:42 -0500

    default page updated

--- reactpro/src/component/Default.js
+++ reactpro/src/component/Default.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 
 export default class Default extends Component{
-	render(){
+	render() {
+
 		return(
 			<div className="height-full text-capitalize ml-5 mt-5">
 				<h3> page not found </h3>
-
+				<h3> The requested URL&nbsp;
+					<span className="text-danger">{this.props.location.pathname}</span>
+					 &nbsp;was not found.
+				</h3>
 			</div>
 
 			)
